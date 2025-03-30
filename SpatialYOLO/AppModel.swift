160b6d18ceedb6d5b9549eadbb0a26954a6431dc
gunner <gymgunner@gmail.com>
2025-03-30 14:14:39 +0800

    Initial Commit

---
+++ SpatialYOLO/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  SpatialYOLO
+//
+//  Created by 关一鸣 on 2025/3/30.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ SpatialYOLO/SpatialYOLOApp.swift
@@ -0,0 +1,33 @@
+//
+//  SpatialYOLOApp.swift
+//  SpatialYOLO
+//
+//  Created by 关一鸣 on 2025/3/30.
+//
+
+import SwiftUI
+
+@main
+struct SpatialYOLOApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
